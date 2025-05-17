84691a2be5320780893036daf9e588c0dc989abd
EyalA6104 <152385011+EyalA6104@users.noreply.github.com>
2025-05-17 13:13:26 +0300

    added popup button asking for the number of square

--- javascript.js
+++ javascript.js
@@ -1,15 +1,33 @@
 const container = document.querySelector("div");
-function getSquares() {
-    for (let i = 0; i < 256; i++) {
-        const square = document.createElement("div")
-        square.classList.add("grid-square")
-        container.appendChild(square)
+const chooseBtn = document.querySelector("button");
 
-        square.addEventListener("mouseenter", () => {
-            square.style["background-color"] = "black";
+chooseBtn.addEventListener("click", () => {
+    let userInput = prompt("Write a number between 1-100");
+    let size = parseInt(userInput);
+
+    if (isNaN(size) || size < 1 || size > 100) {
+        alert("Please choose a number between 1-100");
+        return;
+    }
+
+    function createGrid() {
+    container.innerHTML = "";
+
+    for (let i = 0; i < size * size; i++) {
+        const gridSquare = document.createElement("div")
+        gridSquare.classList.add("grid-square")
+        container.appendChild(gridSquare)
+        let squareSize = 960 / size;
+        gridSquare.style.height = `${squareSize}px`
+        gridSquare.style.width = `${squareSize}px`
+
+        gridSquare.addEventListener("mouseenter", () => {
+            gridSquare.style["background-color"] = "black";
         });
     };
-};
+ };
+ 
+ createGrid()
 
-getSquares()
+});
 
